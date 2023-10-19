9443595ffba3f5076009c017bd28af6d908f68c5
bagas <bagasmuhamad15@gmail.com>
2023-10-19 14:24:47 +0700

    first

---
+++ SimpleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+// Butuh Framework PHPUnit
+use PHPUnit\Framework\TestCase;
+ 
+//Class yang mau di TEST.
+ require_once "Wordcount.php";
+
+ // Class untuk run Testing.
+class SimpleTest extends TestCase
+{
+    public function testCountWords ()
+
+    {
+
+
+// Kita pakai class yang mau kita test.
+$Wc = new WordCount();
+
+// Kita masukan parameter 4 kata, yang harusnya dapat output 4.
+$TestSentence = "name is Joko"; // 4 Kata ..
+$WordCount = $Wc->countWords ($TestSentence);
+
+// Kita assert equal, ekspektasi nya harus 4, jika benar berarti Wordcount berfungsi dengan baik. 
+$this->assertEquals (4, $WordCount);
+}
+}
